0b3aa83c2451e0b37ab9a869528db57eafe094f3
Belinda Brown <belindabrownr04@gmail.com>
2020-03-15 19:32:05 -0600

    Update DefinicionClasicaProbabilidad.py

--- Python/DefinicionClasicaProbabilidad.py
+++ Python/DefinicionClasicaProbabilidad.py
@@ -16,14 +16,4 @@ h = len(numeros_pares)
 #considera la probabilidad de numero pares, cantidad de numeros pares/numero total
 probabilidadpares = float(h)/n
 print("Probabilidad de que salga un número par:   ", probabilidadpares)
-####Parte 2 ejemplo de naipe con 52 cartas probabilidad de sacar 6 con 4 palos 
-# Crear espacio de muestras de palo, es decir un mazo de reinas,
-#diamantes, etc en total son 4
-palo = [i+1 for i in range(13)]
-# Comprobar divisibilidad por 6
-divisible_6 = [i for i in palo if i % 6 is 0]
-print("Tomando en cuenta que hay cuatro palos, la frecuencia relativa de los números divisibles por 6 es:")
-#Saca la probabilidad de que salga 6
-probabilidad_6 = (4*float(len(divisible_6)))/(4*len(palo))
-# Respuesta frecuencia relativa de los números divisibles por 6 es
-print("{0:0.2f}".format(probabilidad_6))
+
